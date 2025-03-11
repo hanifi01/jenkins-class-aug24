3b327dd9d195e9a5bdabe78b6b426414e1dbef1e
hanifi01 <harrisgold01@gmail.com>
2025-03-11 17:01:57 -0500

    post added

--- terraform.groovy
+++ terraform.groovy
@@ -1,75 +1,44 @@
-// pipeline{
-//     agent any
+pipeline {
+    agent any
 
-//     options{
-//       buildDiscarder(logRotator(numToKeepStr: '3'))
-//       retry(2)
-//       timeout(time: 1, unit: 'MINUTES')
-//       ansiColor('xterm')
-//     }
+    stages {
+        stage('tf-init') {
+            steps {
+                dir('infra') {
+                    sh 'terraform init'
+                }
+            }
+        }
 
-//     parameters{
-//         // string(name: 'TF_ACTION', description: "Terraform action", defaultValue: '')
-//         booleanParam(name: 'IS_PROD', description: 'Environment:', defaultValue: true)
-//         choice(name: 'CHOICE_PARAM', description: 'Select a choice:', choices: ['Terraform', 'Cloudformation', 'Plumi'])
-//         choice(name: 'TF_ACTION', description: 'Select Terraform action:', choices: ['apply', 'destroy'])
-//     }
+        stage('tf-check') {
+            steps {
+                dir('infra') {
+                    sh 'terraform fmt && terraform validate'
+                }
+            }
+        }
 
-//     stages{
-//         stage('init'){
-//           steps{
-//                 dir('infra'){
-//                     sh 'terraform init'
-//                     echo "Selected choice is: ${params.CHOICE_PARAM}"
-//                 }
-//           }
-//         }
-//         stage('tf-check'){
-//           steps{
-//                 dir('infra'){
-//                     sh 'terraform fmt; terraform validate'
-//                 }
-//           }
-//         }
-//         stage('plan'){
-//           steps{
-//                 dir('infra'){
-//                     sh 'terraform plan'
-//                 }
-//               }
-//           }
-        
-//         stage('execute'){
-//           steps{
-//                 dir('infra'){
-//                     echo "Is this Production Environment? ${params.IS_PROD}"
-//                     input message: 'Do you want to approve the deployment?', ok: 'Yes'
-//                     sh "terraform ${params.TF_ACTION} -auto-approve"
-//                 }
-//             }
-//         }
-//     }
-//     post {
-//     success {
-//         echo 'Infra has been build successfully'
-//     }
-//     failure {
-//         echo 'Infra build failed'
-//     }
-//     aborted {
-//         echo 'Pipeline aborted!'
-//     }
-//     cleanup { // always executes
-//         echo 'Pipeline build finished and cleaning up .....'
-//         cleanWs()
-//         dir("${workspace_tmp}") {
-//             deleteDir()
-//         }
-//     }
-// }
-// }
+        stage('tf-plan') {
+            steps {
+                dir('infra') {
+                    sh 'terraform apply -auto-approve'
+                }
+            }
+        }
+    }
 
-
-
-
-
+    post {
+        success {
+            echo 'Infra build successful'
+        }
+        failure {
+            echo 'Infra build failed'
+        }
+        aborted {
+            echo 'Pipeline aborted'
+        }
+        always {
+            echo 'Pipeline build completed and cleaning up'
+        }
+    }
+}
