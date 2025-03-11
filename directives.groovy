8612b0d32f2971b297d7f146dd8e2d5e61fb7209
hanifi01 <harrisgold01@gmail.com>
2025-03-11 18:49:24 -0500

    virable added

--- directives.groovy
+++ directives.groovy
@@ -8,18 +8,21 @@ pipeline {
         stage('Build') {
             steps {
                 sh 'echo building > build.txt'
-                echo "This is ${vstisblr1}" // Correct reference to environment variable
+                echo "This is ${env.vstisblr1}" // Correct reference to the environment variable
             }
         }
         stage('Test') {
             steps {
                 sh 'ls -la'
-                echo "This is ${variable2}" // Correct reference to environment variable
+                echo "This is ${env.variable2}" // Correct reference to the environment variable
             }
         }
         stage('Deploy') {
             steps {
-                sh 'echo deploying to prod'
+                echo "Deploying to prod"
+                echo "Job name is ${env.JOB_NAME}" // Correct reference for environment variable
+                echo "Job URL is ${env.JOB_URL}" // Corrected spelling: job_url
+                echo "Git URL: ${env.GIT_URL}" // Corrected spelling: git_url
             }
         }
     }
