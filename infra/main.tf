14b659c8f79772dc509f8c4c96e4127d8b02b179
hanifi01 <harrisgold01@gmail.com>
2025-03-11 17:16:53 -0500

    subnet added

--- infra/main.tf
+++ infra/main.tf
@@ -2,9 +2,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "default" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-00c257e12d6828491"
   instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.default.id
 
   tags = {
     Name = "ec2-created-by-jenkins"
