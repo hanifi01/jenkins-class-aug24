c8ba739994bf4b3215ff70dd639899a477d827cd
hanifi01 <harrisgold01@gmail.com>
2025-03-11 18:33:35 -0500

    virable added

--- directives.groovy
+++ directives.groovy
@@ -2,17 +2,19 @@ pipeline {
     agent any
     environment {
         vstisblr1 = "Building stage"
+        variable2 = "Testing Stage"
     }
     stages {
         stage('Build') {
             steps {
                 sh 'echo building > build.txt'
-                echo "This is ${env.vstisblr1}" // Using the defined environment variable
+                echo "This is ${vstisblr1}" // Correct reference to environment variable
             }
         }
         stage('Test') {
             steps {
                 sh 'ls -la'
+                echo "This is ${variable2}" // Correct reference to environment variable
             }
         }
         stage('Deploy') {
