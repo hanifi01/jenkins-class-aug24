2dc37c0cb171735fc523ac3d0d89c81fd2a3e6e1
hanifi01 <harrisgold01@gmail.com>
2025-03-12 14:49:10 -0500

    paramter added

--- directives.groovy
+++ directives.groovy
@@ -3,26 +3,35 @@ pipeline {
     environment {
         vstisblr1 = "Building stage"
         variable2 = "Testing Stage"
+        jenkinsprivate = credentials('jenkins-private-key') // Removed extra spaces in the credentials call
+    }
+    parameters {
+        string(name: 'BATCH', description: 'Akumo DevOps Batch', defaultValue: '') // Corrected syntax and fixed typos
     }
     stages {
+        stage('Info') {
+            steps {
+                echo "Current DevOps Batch in Akumo is ${params.BATCH}" // Corrected params reference
+            }
+        }
         stage('Build') {
             steps {
                 sh 'echo building > build.txt'
-                echo "This is ${env.vstisblr1}" // Correct reference to the environment variable
+                echo "This is ${env.vstisblr1}" // Correct reference to environment variable
             }
         }
         stage('Test') {
             steps {
                 sh 'ls -la'
-                echo "This is ${env.variable2}" // Correct reference to the environment variable
+                echo "This is ${env.variable2}" // Correct reference to environment variable
             }
         }
         stage('Deploy') {
             steps {
                 echo "Deploying to prod"
                 echo "Job name is ${env.JOB_NAME}" // Correct reference for environment variable
-                echo "Job URL is ${env.JOB_URL}" // Corrected spelling: job_url
-                echo "Git URL: ${env.GIT_URL}" // Corrected spelling: git_url
+                echo "Job URL is ${env.JOB_URL}" // Corrected variable name
+                echo "Git URL: ${env.GIT_URL}" // Corrected variable name
             }
         }
     }
